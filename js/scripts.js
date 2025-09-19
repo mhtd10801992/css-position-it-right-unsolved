f9ce377fb65ff56fee830b79d1709764d60a5314
mhtd10801992 <mhtdas8@gmail.com>
2025-09-18 21:33:36 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const agreeBtn = document.querySelector('.agree-btn');
-  const boxScroll = document.querySelector('.box-scroll');
+const agreeBtn = document.querySelector('.agree-btn');
+const main = document.querySelector('main');
+const footerHeight = parseInt(
+  window.getComputedStyle(document.getElementById('footer')).height
+);
 
-  boxScroll.addEventListener('scroll', () => {
-    const scrollTop = boxScroll.scrollTop;
-    const scrollHeight = boxScroll.scrollHeight;
-    const clientHeight = boxScroll.clientHeight;
-
-    if (scrollTop + clientHeight >= scrollHeight - 1) {
-      agreeBtn.disabled = false;
-      agreeBtn.classList.add('active');
-    } else {
-      agreeBtn.disabled = true;
-      agreeBtn.classList.remove('active');
-    }
-  });
+main.addEventListener('scroll', () => {
+  if (
+    main.scrollTop + main.clientHeight + footerHeight >=
+    main.scrollHeight
+  ) {
+    agreeBtn.removeAttribute('disabled');
+  } else {
+    agreeBtn.setAttribute('disabled', 'true');
+  }
 });
