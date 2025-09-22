8ec31f3355f789d340a7011eb80ab288f1004a14
mhtd10801992 <mhtdas8@gmail.com>
2025-09-21 20:59:38 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 const agreeBtn = document.querySelector('.agree-btn');
-const main = document.querySelector('main');
-const footerHeight = parseInt(
-  window.getComputedStyle(document.getElementById('footer')).height
-);
+const footerHeight = window.getComputedStyle(
+  document.getElementById('footer')
+).height;
 
-main.addEventListener('scroll', () => {
-  if (
-    main.scrollTop + main.clientHeight + footerHeight >=
-    main.scrollHeight
-  ) {
-    agreeBtn.removeAttribute('disabled');
-  } else {
-    agreeBtn.setAttribute('disabled', 'true');
-  }
+document.addEventListener('scroll', () => {
+  window.innerHeight + window.scrollY + parseInt(footerHeight) >=
+  document.body.scrollHeight
+    ? agreeBtn.removeAttribute('disabled')
+    : agreeBtn.setAttribute('disabled', `true`);
 });
